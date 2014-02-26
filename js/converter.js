f580eb20648bb4c73aa3e8c494a1623d0daa9424
owenx1@github.com <Owen.Byrne@student.ncirl.ie>
2014-02-26 20:32:55 +0000

    First Commit

---
+++ js/converter.js
@@ -0,0 +1,28 @@
+﻿var converter = (function () {
+    // private stuff goes here
+
+
+    return {
+
+
+        convertFromImperialToMetric: function (weight) {
+            if (isNaN(weight))
+            {
+                throw new Error("Not a number");         
+            }
+            else
+            {
+                return weight = weight / 2.2;
+            }
+        },
+
+        convertFromMetricToImperial: function (weight) {
+
+           
+        }
+
+    }
+
+
+})();
+
